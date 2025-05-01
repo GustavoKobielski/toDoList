3f85a111876d0d00167834d33e86272db5e20981
Vinicius Codogno <vilecodo@gmail.com>
2025-05-01 02:20:49 -0300

    feat: get, post endpoints for cards and users

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,39 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+card = {
+    1: {
+        "title": "Criar API",
+        "description": "API utilizando fastapi",
+        "author": "Vinicius",
+    },
+    2: {
+        "title": "Merge",
+        "description": "Branch Teste",
+        "author": "Gustavo",
+    }
+}
+
+users = []
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def initial():
+    return card 
+
+@app.get("/get-card/{id_card}")
+def get_card(id_card:int):
+    return card[id_card]
+
+@app.post("/post-user")
+async def create_user(user: User):
+    users.append(user)
+    print(users)
+    return users
