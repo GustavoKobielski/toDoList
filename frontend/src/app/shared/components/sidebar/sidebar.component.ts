4599bd31550e7d947d4e72a103cc5ef5cbb8f92c
Gustavo Kobielski <gust.kobielski@gmail.com>
2025-05-01 01:33:29 -0300

    feature: Sidebar component

---
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+import { SIDEBAR_ITEMS } from '../../const/sidebar.const'
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+  public readonly SIDEBAR_ITENS = SIDEBAR_ITEMS
+}
---
+++ frontend/src/app/shared/const/sidebar.const.ts
@@ -0,0 +1,18 @@
+export const SIDEBAR_ITEMS = [
+  {
+    title: 'Home',
+    route: '/',
+  },
+  {
+    title: 'Tasks',
+    route: '/tasks',
+  },
+  {
+    title: 'Users',
+    route: '/users',
+  },
+  {
+    title: 'Settings',
+    route: '/settings',
+  },
+]
