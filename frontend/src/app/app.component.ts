62cbcafb2f6d434570936a624ab5324b5d41f973
Gustavo Kobielski <gust.kobielski@gmail.com>
2025-05-01 01:43:32 -0300

    feat: restructure app layout and add HomeComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component'
+import { HomeComponent } from './src/app/feature/todo/pages/home/home.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
