22ab570ad9904e4023083ba88c84ec3bc709b358
Gustavo Kobielski <gustavo.k@upflux.net>
2025-05-20 14:03:27 -0300

    feat: enhance sidebar component with icons and fix HTML formatting

--- frontend/src/app/shared/components/sidebar/const/sidebar.const.ts
+++ frontend/src/app/shared/components/sidebar/const/sidebar.const.ts
@@ -2,17 +2,21 @@ export const SIDEBAR_ITEMS = [
   {
     title: 'Home',
     route: '/',
+    icon: 'home',
   },
   {
     title: 'Tasks',
     route: '/tasks',
+    icon: 'menu',
   },
   {
     title: 'Users',
     route: '/users',
+    icon: 'group',
   },
   {
     title: 'Settings',
     route: '/settings',
+    icon: 'settings',
   },
 ]
