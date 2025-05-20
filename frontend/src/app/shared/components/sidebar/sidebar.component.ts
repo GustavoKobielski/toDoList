d54d975fef81ba3195808705572bc154f2c300d3
Gustavo Kobielski <gustavo.k@upflux.net>
2025-05-20 15:42:47 -0300

    feat: add search input component and integrate it into the sidebar

--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { SIDEBAR_ITEMS } from './const/sidebar.const'
+import { SearchInputComponent } from '../search-input/search-input.component'
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SearchInputComponent],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
